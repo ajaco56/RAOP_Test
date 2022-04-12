9d97bb671d4688020d77ce8b8b6a1c6f56c87067
Andreas Jacobs <ajaco@xs4all.nl>
2022-04-12 23:21:27 +0200

    Initial Commit

---
+++ tst9/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tst9
+//
+//  Created by CDust Creative Engineering on 17/09/2020.
+//  Copyright © 2020 CDust Creative Engineering. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
